0cdd241de572efd10e98a857404de6635d822d9c
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-10 12:59:48 +0300

    completed my fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => {
+        if (!response.ok) {
+          // If the response status is not OK,create and throw an error
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // Parse the response to JSON
+        return response.json(); 
+      })
+      // Call renderBooks with the parsed data
+      .then(data => renderBooks(data)) 
+      // Handle any errors
+      .catch(error => console.error('Error fetching books:', error)); 
+  }
   
-}
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
